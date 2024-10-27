58ac14476ad21e50732f480197519e0a29bbc6db
rudeore-098 <rudeore0928@gmail.com>
2024-10-28 00:26:22 +0900

    practice

---
+++ src/main/java/umc/spring/domain/mapping/MemberMission.java
@@ -0,0 +1,24 @@
+package umc.spring.domain.mapping;
+
+import jakarta.persistence.*;
+import lombok.*;
+import umc.spring.domain.Store;
+import umc.spring.domain.common.BaseEntity;
+import umc.spring.domain.enums.MissionStatus;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class MemberMission extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private MissionStatus status;
+}
