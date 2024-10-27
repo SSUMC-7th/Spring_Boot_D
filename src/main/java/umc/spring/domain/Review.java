aa7aa8cd040029b6d6e9b6273df00fde7854d135
rudeore-098 <rudeore0928@gmail.com>
2024-10-28 00:26:22 +0900

    practice

---
+++ src/main/java/umc/spring/domain/Review.java
@@ -0,0 +1,25 @@
+package umc.spring.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CascadeType;
+import umc.spring.domain.common.BaseEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Review extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private Float score;
+}
