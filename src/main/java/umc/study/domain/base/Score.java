c81f7bf9372aa604949458b0d9f9be1dfd8626a8
Yu Jihun <137899326+JihuN126@users.noreply.github.com>
2024-11-04 20:36:13 +0900

    Revert "[Juni] Chapter 5. JPA 기초 및 프로젝트 구조"

--- src/main/java/umc/study/domain/base/Score.java
+++
@@ -1,5 +0,0 @@
-package umc.study.domain.base;
-
-public enum Score {
-    one,two,three,four,five
-}
--- src/main/java/umc/study/domain/enums/Favorite_Food.java
+++
@@ -1,5 +0,0 @@
-package umc.study.domain.enums;
-
-public enum Favorite_Food {
-    A,B,C,D,E
-}
