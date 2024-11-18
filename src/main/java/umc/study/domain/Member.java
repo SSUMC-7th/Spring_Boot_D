fc32c78638b353ef11d4e76a75be424025058dea
BAEK0111 <baekjw111@gmail.com>
2024-11-18 20:16:42 +0900

    [mission/#08] - 미션 수행 (기본 미션)

--- src/main/java/umc/study/domain/Member.java
+++ src/main/java/umc/study/domain/Member.java
@@ -3,6 +3,8 @@ package umc.study.domain;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
 import umc.study.domain.base.BaseEntity;
 import umc.study.domain.enums.Gender;
 import umc.study.domain.enums.MemberStatus;
@@ -17,6 +19,8 @@ import java.util.List;
 
 @Entity
 @Getter
+@DynamicUpdate
+@DynamicInsert
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
@@ -49,7 +53,7 @@ public class Member extends BaseEntity {
     @Enumerated(EnumType.STRING)
     private SocialType socialType;
 
-    @Column(nullable = false, length = 50)
+//    @Column(nullable = false, length = 50)
     private String email;
 
     @ColumnDefault("0")
---
+++ src/main/java/umc/study/web/dto/MemberResponseDTO.java
@@ -0,0 +1,29 @@
+package umc.study.web.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class MemberResponseDTO {
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class JoinResultDTO{
+        Long memberId;
+        LocalDateTime createdAt;
+        Integer gender;
+        Integer age;
+        String address;
+        String specAddress;
+        String name;
+        String email;
+        String phone;
+        List<Long> memberPreferList;
+    }
+}
