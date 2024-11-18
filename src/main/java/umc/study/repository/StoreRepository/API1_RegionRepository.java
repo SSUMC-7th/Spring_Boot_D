dfd3c5d620dc2dcc64340cac2990d9f08b44c5b5
JihuN126 <dbwlgns1208@naver.com>
2024-11-18 18:26:15 +0900

    mission#4

--- src/main/java/umc/study/repository/StoreRepository/API1_RegionRepository.java
+++
@@ -1,7 +0,0 @@
-package umc.study.repository.StoreRepository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import umc.study.domain.Region;
-
-public interface API1_RegionRepository extends JpaRepository<Region, Long> {
-}
