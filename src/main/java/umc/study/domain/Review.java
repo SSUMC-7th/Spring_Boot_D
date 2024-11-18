91de93fac80c80759b15af4153759cf6eb38b913
JihuN126 <dbwlgns1208@naver.com>
2024-11-18 19:03:22 +0900

    mission#4

---
+++ src/main/java/umc/study/domain/Review.java
@@ -0,0 +1,31 @@
+package umc.study.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import umc.study.domain.common.BaseEntity;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Review extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String content;
+
+    private Float score;
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne
+    @JoinColumn(name = "store_id")
+    private Store store;
+
+}
