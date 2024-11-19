848306a86018788bacdc489d81844728d6f7e14c
JihuN126 <dbwlgns1208@naver.com>
2024-11-19 15:17:17 +0900

    Mission8

--- src/main/java/umc/study/web/dto/API4_MemberMissionRequestDTO.java
+++ src/main/java/umc/study/web/dto/API4_MemberMissionRequestDTO.java
@@ -6,6 +6,6 @@ import lombok.Setter;
 @Getter
 @Setter
 public class API4_MemberMissionRequestDTO {
-    private Long memberId; // 도전할 회원의 ID
-    private Long missionId; // 도전할 미션의 ID
+    private Long memberId;
+    private Long missionId;
 }
