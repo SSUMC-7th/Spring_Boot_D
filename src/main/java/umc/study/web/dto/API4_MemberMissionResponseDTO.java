5903303a5dc9fd043e5b2ca436863808cb558769
JihuN126 <dbwlgns1208@naver.com>
2024-11-19 15:42:21 +0900

    mission8

--- src/main/java/umc/study/web/dto/API4_MemberMissionResponseDTO.java
+++ src/main/java/umc/study/web/dto/API4_MemberMissionResponseDTO.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public class API4_MemberMissionResponseDTO {
-    private Long id; // MemberMission ID
+    private Long id;
     private Long memberId;
     private Long missionId;
     private String status;
