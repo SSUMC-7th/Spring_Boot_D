702d953daa55498efe2e92de3f9327812ca2fc81
BAEK0111 <baekjw111@gmail.com>
2024-12-02 23:31:01 +0900

    [practice/#10] - 실습2 완료

--- src/main/java/umc/study/config/security/CustomOAuth2UserService.java
+++ src/main/java/umc/study/config/security/CustomOAuth2UserService.java
@@ -1,11 +1,13 @@
 package umc.study.config.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
 import umc.study.domain.Member;
 import umc.study.domain.enums.Gender;
 import umc.study.domain.enums.Role;
@@ -15,14 +17,11 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
+@Service
+@RequiredArgsConstructor
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
-
-    public CustomOAuth2UserService(MemberRepository memberRepository, PasswordEncoder passwordEncoder) {
-        this.memberRepository = memberRepository;
-        this.passwordEncoder = passwordEncoder;
-    }
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
